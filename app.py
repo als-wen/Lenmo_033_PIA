f07c99ddc4fd81e0f4d7041aaf169ed189853fd7
alan-vglez <alan.avglez@gmail.com>
2024-11-11 12:34:30 -0600

    feat: CRUD for basketball team player management

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import os
+import MySQLdb.cursors
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'flaskpia'
+app.config['MYSQL_PASSWORD'] = '12345pass'
+app.config['MYSQL_DB'] = 'basketball_team'
+
+mysql = MySQL(app)
+
+# Show all players
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM players")
+    players = cursor.fetchall()
+    return render_template('index.html', players=players)
+
+# Add new player
+@app.route('/add', methods=['GET', 'POST'])
+def add_player():
+    if request.method == 'POST':
+        name = request.form['name']
+        height = request.form['height']
+        weight = request.form['weight']
+        age = request.form['age']
+        years_playing = request.form['years_playing']
+        salary = request.form['salary']
+        points_per_game = request.form['points_per_game']
+        rebounds_per_game = request.form['rebounds_per_game']
+        steals_per_game = request.form['steals_per_game']
+        blocks_per_game = request.form['blocks_per_game']
+        turnovers_per_game = request.form['turnovers_per_game']
+        
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            "INSERT INTO players (name, height, weight, age, years_playing, salary, points_per_game, rebounds_per_game, steals_per_game, blocks_per_game, turnovers_per_game) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (name, height, weight, age, years_playing, salary, points_per_game, rebounds_per_game, steals_per_game, blocks_per_game, turnovers_per_game)
+        )
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+# Edit player
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_player(id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST':
+        name = request.form['name']
+        height = request.form['height']
+        weight = request.form['weight']
+        age = request.form['age']
+        years_playing = request.form['years_playing']
+        salary = request.form['salary']
+        points_per_game = request.form['points_per_game']
+        rebounds_per_game = request.form['rebounds_per_game']
+        steals_per_game = request.form['steals_per_game']
+        blocks_per_game = request.form['blocks_per_game']
+        turnovers_per_game = request.form['turnovers_per_game']
+        
+        cursor.execute(
+            "UPDATE players SET name=%s, height=%s, weight=%s, age=%s, years_playing=%s, salary=%s, points_per_game=%s, rebounds_per_game=%s, steals_per_game=%s, blocks_per_game=%s, turnovers_per_game=%s WHERE id=%s",
+            (name, height, weight, age, years_playing, salary, points_per_game, rebounds_per_game, steals_per_game, blocks_per_game, turnovers_per_game, id)
+        )
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+    else:
+        cursor.execute("SELECT * FROM players WHERE id = %s", (id,))
+        player = cursor.fetchone()
+        return render_template('edit.html', player=player)
+
+# Delete player
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete_player(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("DELETE FROM players WHERE id = %s", (id,))
+    mysql.connection.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
